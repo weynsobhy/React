1272d1923063a35c85ead671af95223c5321d631
weynsobhy <weynsobhy@gmail.com>
2017-05-02 22:45:51 +0700

    belajar pertama

---
+++ es-reactjs/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          
+          <h2>Belajar react pertama</h2>
+        </div>
+        <p className="App-intro">
+          Ini keren banget...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
